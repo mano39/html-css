724c09ec6f921cfd49044e7727b8551d476367ac
M Talha <selfcodex@gmail.com>
2025-03-22 10:17:25 +0500

    add js

---
+++ index.js
@@ -0,0 +1,43 @@
+var a ='pny'
+var a = 'arfa'
+
+let b = 'let'
+    b = 'let2'
+const c = 1244
+
+// console.log(b,c)
+// alert('hello dear')
+// const data = prompt("Enter")
+
+console.log(typeof a, typeof c)
+
+const ar = ['pny', 'arfa',1244]
+ar.push('kdfjak')
+console.log(ar)
+
+const obj = {
+    names: 'John',
+    age: 30,
+    city: 'New York'
+}
+
+// console.log(ob.names)
+
+// array
+// Object
+// console.log(...ar)
+const news = [1234,...ar]
+console.log(news)
+// spread operators
+// descructor
+
+let {names, age} = obj
+
+console.log(names, age)
+// alert(names, age)
+
+// function 
+// arrow function
+// condition
+// operators
+// loops
